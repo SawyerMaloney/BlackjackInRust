afe5ff9fd3f09cff314b6d9494cf0936ab290dd9
Sawyer Maloney <sawyerdmaloney@gmail.com>
2024-11-12 14:16:44 -0500

    adding card comparing

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ impl Card {
             value,
         } 
     }
+
     fn suit_to_string(&self) -> String {
         match self.suit {
             Suit::Clubs => String::from("♣"),
@@ -38,9 +39,40 @@ impl Card {
     fn to_string(&self) -> String {
         format!("{0}{1}", self.value_to_string(), self.suit_to_string())
     }
+
+    fn suit_to_int(&self) -> u8 {
+        match self.suit {
+            Suit::Clubs => 0,
+            Suit::Diamonds => 1,
+            Suit::Hearts => 2,
+            Suit::Spades => 3,
+        }
+    }
+
+    // returns true if greater, false if not
+    fn is_greater(&self, card: &Card) -> bool {
+        if self.value > card.value {
+            return true;
+        }
+        else if self.value < card.value {
+            return false;
+        }
+        else {
+            // same value, so compare suit
+            // putting >= because tie goes to user
+            self.suit_to_int() >= card.suit_to_int()
+        }
+    }
 }
 
 fn main() {
-    let c = Card::new(Suit::Clubs, 7);
+    let c = Card::new(Suit::Spades, 7);
+    let b = Card::new(Suit::Hearts, 7);
     println!("{}", c.to_string());
+    if c.is_greater(&b) {
+        println!("{0} is greater than {1}", c.to_string(), b.to_string());
+    }
+    else if b.is_greater(&c) {
+        println!("{0} is greater than {1}", b.to_string(), c.to_string());
+    }
 }
